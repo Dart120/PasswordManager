cacd680008833392e4820755d99a3ce7df9261ae
Dart120 <Irenitemi81@gmail.com>
2019-11-14 21:01:04 +0000

    Added ablity to remove a password

--- passwordlocker.py
+++ passwordlocker.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 """An insecure python program"""
 
-passwords = {'email':'101qpq101101', 'steam': 'waletinu1@', 'clubpenguin': '101101'}
+passwords = {'email':'heydudes101', 'steam': 'password', 'clubpenguin': '101101'}
 import sys, pyperclip
 def retrivepassword():
     if len(sys.argv) < 2:
@@ -16,12 +16,28 @@ def retrivepassword():
         print('There is no account named: %s' % account)
         sys.exit()
 def addpassword():
-    if len(sys.argv) < 2:
-        print('Usage: python passwordlocker.py [account] [password] - copy account password')
+    if len(sys.argv) < 3:
+        print('Usage: python passwordlocker.py [account] [password] - adds account to database')
         sys.exit()
     account = sys.argv[1]
     password = sys.argv[2]
-    passwords[account]=password
+    passwords[account] = password
     print("Account %s added to database" % account)
     sys.exit()
+
+def removepassword():
+    if len(sys.argv) < 3:
+        print('Usage: python passwordlocker.py [account] [password] - removes account from database')
+        sys.exit()
+    account = sys.argv[1]
+    password = sys.arv[2]
+    for k, v in passwords.items():
+        if passwords.has_key(k) and v in passwords.values():
+            del passwords[k]
+            print("Account %s removed from database" % account)
+            
+    
+
+
+
     
